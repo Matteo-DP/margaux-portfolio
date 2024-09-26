d281ac4c7fb53a618b45cb36e35507ee21d9ee39
Matteo De Pauw <matteodepauw@yahoo.com>
2024-09-26 14:49:45 +0200

    sort artworks based on sort parameter

--- src/services/pbService.js
+++ src/services/pbService.js
@@ -20,9 +20,9 @@ export async function getArt(collection) {
             const collectionId = collections.filter(e => e.handle == collection)[0].id
             const art = await pb.collection('art').getFullList({
                 filter: `collection = '${collectionId}'`,
-                sort: 'updated',
+                sort: 'sort',
             })
-            return JSON.parse(JSON.stringify(art)).reverse();
+            return JSON.parse(JSON.stringify(art));
         } catch {
             return { 
                 code: 404,
